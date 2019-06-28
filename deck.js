cd31be1eece397ce73bdac3ee52618dee54e0c71
Lukas Sadzik <lukas.sadzik@sensiolabs.de>
2019-06-28 13:14:58 +0200

    Finished role chapter, little cleanup, split deck.mdx

---
+++ deck.js
@@ -0,0 +1,7 @@
+import { slides as d01 } from './deck/01_intro.mdx'
+import { slides as d02 } from './deck/02_quickstart.mdx'
+import { slides as d03 } from './deck/03_roles.mdx'
+import { slides as d04 } from './deck/04_inventory.mdx'
+export { githubFull  as theme } from "code-surfer/themes"
+
+export const slides = [...d01, ...d02, ...d03, ...d04]
