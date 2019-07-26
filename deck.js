8911ee92887bd2b5fc2b457004a920737ae7b477
ko <>
2019-07-26 16:39:01 +0200

    First draft of a finished talk

--- deck.js
+++ deck.js
@@ -2,6 +2,10 @@ import { slides as d01 } from './deck/01_intro.mdx'
 import { slides as d02 } from './deck/02_quickstart.mdx'
 import { slides as d03 } from './deck/03_roles.mdx'
 import { slides as d04 } from './deck/04_inventory.mdx'
+import { slides as d05 } from './deck/05_roles_advanced.mdx'
+import { slides as d06 } from './deck/06_playbooks.mdx'
+import { slides as d07 } from './deck/07_outro.mdx'
+import { slides as d99 } from './deck/99_end.mdx'
 export { githubFull  as theme } from "code-surfer/themes"
 
-export const slides = [...d01, ...d02, ...d03, ...d04]
+export const slides = [...d01, ...d02, ...d03, ...d04, ...d05, ...d06, ...d07, ...d99]
